83352a164df3d9e6df126ec68ab63821c68f2a8a
Olga <olchiknega@mail.ru>
2022-08-21 14:42:54 +0700

    решена задача 35

--- 34-35-36-38/Program.cs
+++ 34-35-36-38/Program.cs
@@ -64,7 +64,20 @@ namespace _34_35_36_38
 
             static void TaskThirtyfive()
             {
+                int[] array = new int[123];
+                Random rand = new Random();
+                int count = 0;
 
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rand.Next(0, 151);
+                    if ((array[i] >= 10) && (array[i] <= 99))
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(string.Join(", ", array));
+                Console.WriteLine(count);
             }
 
             #endregion
